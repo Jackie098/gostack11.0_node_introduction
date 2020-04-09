694e6f8f712e62b9c7ccf22777b6e3abdf782e92
Jackie098 <carlosmiranda19122@gmail.com>
2020-04-09 04:02:36 -0300

    created full CRUD

---
+++ src/index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const { uuid } = require('uuidv4');
+
+const app = express();
+
+app.use(express.json());
+
+const projects = [];
+
+app.get('/projects', (req, res) => {
+  const { title } = req.query;
+
+  const results = title
+    ? projects.filter(project => project.title.includes(title))
+    : projects;
+
+  return res.json(results);
+});
+
+app.post('/projects', (req, res) => {
+  const {title, owner} = req.body;
+
+  const project = { id: uuid(), title, owner};
+
+  projects.push(project);
+
+  return res.json(project);
+});
+
+app.put('/projects/:id', (req, res) => {
+  const {id} = req.params;
+  const {title, owner} = req.body;
+
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  if(projectIndex < 0) {
+    return res.status(400).json({error : 'Project not found'});
+  }
+
+  const project = {
+    id,
+    title,
+    owner,
+  };
+
+  projects[projectIndex] = project;
+
+  return res.json(project);
+});
+
+app.delete('/projects/:id', (req, res) => {
+  const {id} = req.params;
+
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  if(projectIndex < 0) {
+    return res.status(400).json({error : 'Project not found'});
+  }
+
+  projects.splice(projectIndex, 1);
+
+  return res.status(204).send();
+});
+
+
+app.listen(3333, () => {
+  console.log('server running');
+})
